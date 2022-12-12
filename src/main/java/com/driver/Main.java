599def22e9455a66cf5edad9e15f87bcd755d4e4
unknown <mohd.althaf337@gmail.com>
2022-12-12 11:24:58 +0530

    msg1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.variable = "heyhey;
+        obj.setVariable("heyheyhey");
+        System.out.println(obj.getVariable());
+    }
 }
+
+
+
+
